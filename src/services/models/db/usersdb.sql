1db6e40fea7b496f8e02ea3bae3d4c7d73a10497
kpawn92 <hap921020@gmail.com>
2023-02-02 08:43:48 -0500

    database

--- src/services/models/db/usersdb.sql
+++ src/services/models/db/usersdb.sql
@@ -11,11 +11,27 @@
  Target Server Version : 100419
  File Encoding         : 65001
 
- Date: 01/02/2023 16:51:47
+ Date: 02/02/2023 08:42:46
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for tb_moderator
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_moderator`;
+CREATE TABLE `tb_moderator`  (
+  `id` varchar(36) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `name` varchar(25) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `lastname` varchar(50) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `userId` varchar(36) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_spanish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_moderator
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for tb_references
@@ -41,14 +57,14 @@ CREATE TABLE `tb_role`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(11) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 12 CHARACTER SET = utf8 COLLATE = utf8_spanish_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 18 CHARACTER SET = utf8 COLLATE = utf8_spanish_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of tb_role
 -- ----------------------------
-INSERT INTO `tb_role` VALUES (9, 'root');
-INSERT INTO `tb_role` VALUES (10, 'moderator');
-INSERT INTO `tb_role` VALUES (11, 'user');
+INSERT INTO `tb_role` VALUES (15, 'root');
+INSERT INTO `tb_role` VALUES (16, 'user');
+INSERT INTO `tb_role` VALUES (17, 'moderator');
 
 -- ----------------------------
 -- Table structure for tb_subscriber
@@ -58,7 +74,7 @@ CREATE TABLE `tb_subscriber`  (
   `id` varchar(36) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   `name` varchar(25) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   `lastname` varchar(50) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
-  `user_id` varchar(36) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `userId` varchar(36) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_spanish_ci ROW_FORMAT = Dynamic;
 
@@ -76,12 +92,13 @@ CREATE TABLE `tb_user`  (
   `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
   `role` int(11) NOT NULL,
   `createdAt` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE CURRENT_TIMESTAMP(6),
+  `state` tinyint(1) NOT NULL DEFAULT 1,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_spanish_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of tb_user
 -- ----------------------------
-INSERT INTO `tb_user` VALUES ('23cb3930-a0c7-4c9d-8428-e9486a258228', 'admin@admin.com', '$2a$10$r.EXtKS7LOPj0bSjwLKaQe0Ou9tPnI0wCnv0VzpKgBYEIzRieNwzS', 9, '2023-02-01 15:59:18.803899');
+INSERT INTO `tb_user` VALUES ('2434c0d3-b806-41ef-ad31-a1946b199f53', 'admin@admin.com', '$2a$10$Szb3G91wTWzvAW3M0i5/rugIZxjaEh5ThImgU7jvp7MpsRHW33YfS', 15, '2023-02-01 18:10:44.939138', 1);
 
 SET FOREIGN_KEY_CHECKS = 1;
