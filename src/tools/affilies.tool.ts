e0eb87c3ba7e37aeb63d92edaaaa383ce8dc021d
kpawn92 <hap921020@gmail.com>
2023-02-15 15:43:50 -0500

    revisar tools affilies

--- src/tools/affilies.tool.ts
+++ src/tools/affilies.tool.ts
@@ -2,6 +2,13 @@ import { RowDataPacket } from 'mysql2';
 import { References } from '../services/models';
 
 // Covert el helper en un middleware function(req, res)
+// Analizar la posibilidad de guardar en memoria para que sea agil la ejecucion y sea eficiente la concurrencia
+// Investigar redis.
+/**
+ * Inicio
+ * Realizar la consulta una sola vez
+ * Crear la tabla en redis y al terminar el proceso ejecutar un setTimeOut() para guardar los cambios en mysql
+ */
 
 const updateAffilities = async (affilies: RowDataPacket) => {
     return affilies;
@@ -11,7 +18,7 @@ export const affilies = async (userId: string, idAffilies: string) => {
     const affilies: string = JSON.stringify([{ idAffilies }]);
 
     const affiliesLast = <RowDataPacket>await References.searchSponsor(userId);
-    if (affiliesLast.length === 0) return updateAffilities(affiliesLast);
+    if (affiliesLast.length > 0) return updateAffilities(affiliesLast);
 
     const result = await References.createdReference({ userId, affilies });
     return { result };
