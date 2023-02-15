04718ac96f670f9bce450d269784ba89ea5335c8
kpawn92 <hap921020@gmail.com>
2023-02-15 15:27:19 -0500

    upto

--- src/tools/affilies.tool.ts
+++ src/tools/affilies.tool.ts
@@ -1,10 +1,18 @@
 import { RowDataPacket } from 'mysql2';
 import { References } from '../services/models';
 
+// Covert el helper en un middleware function(req, res)
+
+const updateAffilities = async (affilies: RowDataPacket) => {
+    return affilies;
+};
+
 export const affilies = async (userId: string, idAffilies: string) => {
     const affilies: string = JSON.stringify([{ idAffilies }]);
+
     const affiliesLast = <RowDataPacket>await References.searchSponsor(userId);
-    if (affiliesLast.length > 0) return; // actualizar;
+    if (affiliesLast.length === 0) return updateAffilities(affiliesLast);
+
     const result = await References.createdReference({ userId, affilies });
     return { result };
 };
